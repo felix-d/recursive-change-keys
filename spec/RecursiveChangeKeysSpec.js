f2f35920518c7373b43e81eceb95cf48b3da4732
Felix D <flx.descoteaux@gmail.com>
2015-06-26 08:08:48 -0400

    added tests

---
+++ spec/RecursiveChangeKeysSpec.js
@@ -0,0 +1,46 @@
+var equal = require("deep-equal");
+
+describe("recursive-change-keys", function() {
+  var recursiveChangeKeys = require('../index');
+  var testObj;
+  var changeKeys;
+
+  beforeEach(function() {
+      testObj = {
+          key1: "test", key2: "test",
+          key3: {
+              key1: "test",
+              key4: {
+                  key1: "test",
+                  key5: [{
+                      key1: "test",
+                      key2: "test",
+                      newkey: "notoverridden"
+                  }]
+              }
+          }
+      };
+      changeKeys = {key1: "newkey", key2: "newkey2"};
+  });
+
+  it("it should recursively change the right keys", function() {
+      var success = {
+          newkey: "test",
+          newkey2: "test",
+          key3: {
+              newkey: "test",
+              key4: {
+                  newkey: "test",
+                  key5: [{
+                      key1: "test",
+                      newkey2: "test",
+                      newkey: "notoverridden"
+                  }]
+              }
+          }
+      };
+      recursiveChangeKeys(testObj, changeKeys);
+      expect(equal(testObj, success)).toBeTruthy();
+  });
+
+});
