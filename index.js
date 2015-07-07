48275d67e6e5496bb2f55d77cd6932bf28e88174
Felix Descoteaux <flx.descoteaux@gmail.com>
2015-07-07 11:03:36 -0400

    added maxDepth option

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-var cloneDeep = require("lodash.clonedeep");
+var cloneDeep = require("lodash.clonedeep"),
+    _maxDepth;
 
 function escape(text) {
   return text.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, "\\$&");
-};
+}
 
 /**
  * Traverses an object recursively and substitutes the specified keys for the given values 
@@ -10,13 +11,16 @@ function escape(text) {
  * @param {Object} obj The object in which we want to change the keys
  * @param {Object} keysToChange an object of the form {oldKey1: "newKey1", oldKey2: "newKey2"}
  */
-var _recursiveChangeKeys = function(obj, keysToChange){
+var _recursiveChangeKeys = function(obj, keysToChange, depth){
+  if(depth === _maxDepth){
+    return;
+  }
   if(obj instanceof Object){
     for(var oldkey in obj){
       if(obj.hasOwnProperty(oldkey)){
         for(var keyToChange in keysToChange){
 
-          // If the key in keysToChange is a regular expression
+          // If the key in keysToChange is a replacement
           if(keysToChange[keyToChange].replace){
 
             var keyToChangeRe = new RegExp(escape(keyToChange), 'g');
@@ -36,14 +40,15 @@ var _recursiveChangeKeys = function(obj, keysToChange){
             }
           }
         }
-        _recursiveChangeKeys(obj[oldkey], keysToChange);
+        _recursiveChangeKeys(obj[oldkey], keysToChange, ++depth);
       }
     }
   }
 };
-var recursiveChangeKeys = function(obj, keysToChange){
+var recursiveChangeKeys = function(obj, keysToChange, maxDepth){
+  _maxDepth = maxDepth;
   var newobj = cloneDeep(obj);
-  _recursiveChangeKeys(newobj, keysToChange);
+  _recursiveChangeKeys(newobj, keysToChange, 1);
   return newobj;
 };
 
